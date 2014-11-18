9685fbb7e11a086595ba9edb9aa2e9f18243e438
Ivan Smirnov <ismirnovrus@ya.ru>
2014-11-18 20:38:51 +0300

    fourth problem of the firstworkshop second version

--- algorithm/sliding_window_maximum.cpp
+++ algorithm/sliding_window_maximum.cpp
@@ -4,118 +4,99 @@
 #include <vector>
 #include <iterator>
 
+template <typename ValueType>
+class MaxQueue {
+public:
+    MaxQueue();
+    bool Empty() const;
+    size_t Size() const;
+    const ValueType& Head();
+    const ValueType& Max();
+    void Enqueue(const ValueType& val);
+    void Dequeue();
+private:
+    std::stack<ValueType> tail;
+    std::stack<ValueType> tailMaximum;
+    std::stack<ValueType> head;
+    std::stack<ValueType> headMaximum;
+};
 
-void Enqueue(int element, std::stack<int>* tail, std::stack<int>* tailMaximum) {
-    if (tail->empty()) {
-        tailMaximum->push(element);
-        tail->push(element);
-    } else {
-        tailMaximum->push(std::max(tailMaximum->top(), element));
-        tail->push(element);
-    }
+template <typename ValueType>
+MaxQueue<ValueType>::MaxQueue() {}
+
+template <typename ValueType>
+bool MaxQueue<ValueType>::Empty() const {
+    return tail.empty() && head.empty();
 }
 
-void Dequeue(std::stack<int>* head, std::stack<int>* headMaximum,
-    std::stack<int>* tail, std::stack<int>* tailMaximum) {
-    if (head->empty()) {
-        while (!tail->empty()) {
-            Enqueue(tail->top(), head, headMaximum);
-            tail->pop();
-            tailMaximum->pop();
+template <typename ValueType>
+size_t MaxQueue<ValueType>::Size() const {
+    return tail.size() + head.size();
+}
+
+template <typename ValueType>
+const ValueType& MaxQueue<ValueType>::Head() {
+    if (head.empty()) {
+        while (!tail.empty()) {
+            if (head.empty()) {
+                headMaximum.push(tail.top());
+                head.push(tail.top());
+            } else {
+                headMaximum.push(std::max(headMaximum.top(), tail.top()));
+                head.push(tail.top());
+            }
+            tail.pop();
+            tailMaximum.pop(); 
         }
-        head->pop();
-        headMaximum->pop();
+        return head.top();
     } else {
-        head->pop();
-        headMaximum->pop();
+        return head.top();
     }
 }
 
-int HeadQueue(std::stack<int>* head, std::stack<int>* headMaximum,
-    std::stack<int>* tail, std::stack<int>* tailMaximum) {
-    if (head->empty()) {
-        while (!tail->empty()) {
-            Enqueue(tail->top(), head, headMaximum);
-            tail->pop();
-            tailMaximum->pop();
-        }
-        return head->top();
+template <typename ValueType>
+const ValueType& MaxQueue<ValueType>::Max() {
+    if (tail.empty()) {
+        return headMaximum.top();
     } else {
-        return head->top();
-    }
-}
-
-int TailQueue(std::stack<int>* head, std::stack<int>* headMaximum,
-    std::stack<int>* tail, std::stack<int>* tailMaximum) {
-    if (tail->empty()) {
-        while (!head->empty()) {
-            Enqueue(head->top(), tail, tailMaximum);
-            head->pop();
-            headMaximum->pop();
-        }
-        return tail->top();
-    } else {
-        return tail->top();
-    }
-}
-
-int MaxHeadQueue(std::stack<int>* head, std::stack<int>* headMaximum,
-    std::stack<int>* tail, std::stack<int>* tailMaximum) {
-    if (head->empty()) {
-        while (!tail->empty()) {
-            Enqueue(tail->top(), head, headMaximum);
-            tail->pop();
-            tailMaximum->pop();
-        }
-        return headMaximum->top();
-    } else {
-        return headMaximum->top();
-    }
-}
-
-int MaxTailQueue(std::stack<int>* head, std::stack<int>* headMaximum,
-    std::stack<int>* tail, std::stack<int>* tailMaximum) {
-    if (tail->empty()) {
-        while (!head->empty()) {
-            Enqueue(head->top(), tail, tailMaximum);
-            head->pop();
-            headMaximum->pop();
-        }
-        return tailMaximum->top();
-    } else {
-        return tailMaximum->top();
-    }
-}
-
-int GetMax(std::stack<int>* head, std::stack<int>* headMaximum,
-    std::stack<int>* tail, std::stack<int>* tailMaximum) {
-    if (tail->empty()) {
-        return headMaximum->top();
-    } else {
-        if (head->empty()) {
-            return tailMaximum->top();
+        if (head.empty()) {
+            return tailMaximum.top();
         } else {
-        return std::max(MaxTailQueue(head, headMaximum, tail, tailMaximum),
-        MaxHeadQueue(head, headMaximum, tail, tailMaximum));
+        return std::max(tailMaximum.top(), headMaximum.top());
         }
+    } 
+}
+
+template <typename ValueType>
+void MaxQueue<ValueType>::Enqueue(const ValueType& val) {
+    if (tail.empty()) {
+        tailMaximum.push(val);
+        tail.push(val);
+    } else {
+        tailMaximum.push(std::max(tailMaximum.top(), val));
+        tail.push(val);
     }
 }
 
-void PrintQueue(std::stack<int>* head, std::stack<int>* headMaximum,
-    std::stack<int>* tail, std::stack<int>* tailMaximum) {
-    while (!head->empty() || !tail->empty()) {
-        std::cout << HeadQueue(head, headMaximum, tail, tailMaximum) << std::endl;
-        Dequeue(head, headMaximum, tail, tailMaximum);
-    }
-}
-
-void ReadQueue(std::stack<int>* tail, std::stack<int>* tailMaximum) {
-    size_t size;
-    std::cin >> size;
-    int element;
-    for (size_t elementsCounter = 0; elementsCounter < size; ++elementsCounter) {
-        std::cin >> element;
-        Enqueue(element, tail, tailMaximum);
+template <typename ValueType>
+void MaxQueue<ValueType>::Dequeue() {
+    if (head.empty()) {
+        while (!tail.empty()) {
+            if (head.empty()) {
+                headMaximum.push(tail.top());
+                head.push(tail.top());
+            } else {
+                headMaximum.push(std::max(headMaximum.top(), tail.top()));
+                head.push(tail.top());
+            }
+            tail.pop();
+            tailMaximum.pop(); 
+        }
+        head.pop();
+        headMaximum.pop();
+    } else {
+        head.pop();
+        headMaximum.pop();
     }
 }
 
@@ -136,24 +117,20 @@ int main(int argc, char const *argv[]) {
     ReadVector(&elements);
     size_t commandQuantity;
     std::cin >> commandQuantity;
-    std::stack<int> tail;
-    std::stack<int> tailMaximum;
-    std::stack<int> head;
-    std::stack<int> headMaximum;
+    MaxQueue<int> queue;
     char command;
-    size_t rBorder = 0, lBorder = 0;
-    Enqueue(elements[0], &tail, &tailMaximum);
+    size_t rBorder = 0;
+    queue.Enqueue(elements[0]);
     std::vector<int> maximums;
     for (size_t commandCounter = 0; commandCounter < commandQuantity; ++commandCounter) {
         std::cin >> command;
         if (command == 'R') {
             ++rBorder;
-            Enqueue(elements[rBorder], &tail, &tailMaximum);
-            maximums.push_back(GetMax(&head, &headMaximum, &tail, &tailMaximum));
+            queue.Enqueue(elements[rBorder]);
+            maximums.push_back(queue.Max());
         } else {
-            ++lBorder;
-            Dequeue(&head, &headMaximum, &tail, &tailMaximum);
-            maximums.push_back(GetMax(&head, &headMaximum, &tail, &tailMaximum));
+            queue.Dequeue();
+            maximums.push_back(queue.Max());
         }
     }
     std::copy(maximums.begin(), maximums.end(), std::ostream_iterator<int>(std::cout, " "));
